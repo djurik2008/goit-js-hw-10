ac03f20bccd3d33414bcd88e6178b971ea055d71
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2023-11-25 15:46:07 +0200

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -14,14 +14,16 @@ const refs = {
     errorEl: document.querySelector(".error")
 }
 
-refs.loaderEl.classList.replace('loader', 'is-hidden')
+refs.catInfoBlock.classList.add('is-hidden')
+
 
 fetchBreeds().then(({data}) => {
-    refs.loaderEl.classList.replace('is-hidden', 'loader')
+    refs.loaderEl.classList.replace('loader', 'is-hidden')
+    // refs.loaderEl.classList.replace('is-hidden', 'loader')
     return data
 }).then((resp) => {
     const arrOfOption = breedSelectCreate(resp)
-    // arrOfOption.unshift({placeholder: true, text: 'Select breed'})
+    arrOfOption.unshift({placeholder: true, text: 'Select breed'})
     const select = new SlimSelect({
         select: refs.breedSelectEl,
       
@@ -31,18 +33,20 @@ fetchBreeds().then(({data}) => {
             // allowDeselect: true
         }
     })
-    select.setSelected('Abyssinian')
     refs.loaderEl.classList.replace('loader', 'is-hidden')
 }).catch(onFetchError)
 
 refs.breedSelectEl.addEventListener('change', (e) => {
+    refs.catInfoBlock.classList.add('is-hidden')
+
     refs.loaderEl.classList.replace('is-hidden', 'loader')
     const id = e.currentTarget.value
+    if (id === 'Select breed') return
     
     fetchCatByBreed(id).then(({data}) => {
         const { url, breeds } = data[0];
         refs.catInfoBlock.innerHTML = catInfoMarkupCreate(breeds, url)
         refs.loaderEl.classList.replace('loader', 'is-hidden')
-    }).catch(onFetchError)    
-    
+        refs.catInfoBlock.classList.remove('is-hidden')
+    }).catch(onFetchError)
 })
